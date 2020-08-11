c44b1ca2b2abb5428c9b1f826d05d6dcbd66ce80
DiAntezanaA <nalid102@gmail.com>
2020-08-11 15:46:23 -0400

    ngColor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit} from '@angular/core';
-
+import {from, fromEvent} from 'rxjs'
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ]     
 })
 export class AppComponent implements OnInit{
   title = 'curso-angular';
@@ -13,6 +13,8 @@ export class AppComponent implements OnInit{
   persona = ['marco', 'maria','jorge', 'lupe']
 
   aux = 3;
+
+  auxColor = 'red';
 
   ngOnInit(){
 
